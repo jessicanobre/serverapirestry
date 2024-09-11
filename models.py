e9a80ce83a8177c11ea3abb75e21b4774d5bd007
jessicanobre <150728052+jessicanobre@users.noreply.github.com>
2024-09-11 00:34:08 +0000

    desafio

--- models.py
+++ models.py
@@ -1,9 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
+from database import db  # Use a instância de db existente
 
-# Inicializando o banco de dados
-db = SQLAlchemy()
-
-# Definindo o modelo para o Lead
 class Lead(db.Model):
     __tablename__ = 'leads'
     id = db.Column(db.Integer, primary_key=True)
@@ -12,15 +8,17 @@ class Lead(db.Model):
     longitude = db.Column(db.Float, nullable=False)
     temperature = db.Column(db.Float, nullable=False)
     interest = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    telefone = db.Column(db.String(15), nullable=False)
 
-    def __init__(self, name, latitude, longitude, temperature, interest):
+    def __init__(self, name, latitude, longitude, temperature, interest, email, telefone):
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
         self.temperature = temperature
         self.interest = interest
-
-    # Função para retornar os dados como dicionário
+        self.email = email
+        self.telefone = telefone
     def as_dict(self):
         return {
             'id': self.id,
@@ -28,5 +26,7 @@ class Lead(db.Model):
             'latitude': self.latitude,
             'longitude': self.longitude,
             'temperature': self.temperature,
-            'interest': self.interest
+            'interest': self.interest,
+            'name': self.name,
+            'email' : self.email
         }
